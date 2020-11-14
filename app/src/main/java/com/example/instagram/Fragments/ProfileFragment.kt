f93dcdddcd2686523cc5b46d76eec765b70d4cda
PavanMeshram <pavanmeshram0@gmail.com>
2020-11-14 16:18:58 +0530

    Instagram using Kotlin

--- app/src/main/java/com/example/instagram/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/instagram/Fragments/ProfileFragment.kt
@@ -13,28 +13,7 @@ import com.example.instagram.AccountSettingsActivity
 import com.example.instagram.R
 import com.example.instagram.Fragments.ProfileFragment
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     private var ctx: Context? = null
     private var self: View? = null
@@ -53,25 +32,5 @@ class ProfileFragment : Fragment() {
             startActivity(Intent(context, AccountSettingsActivity::class.java))
         }
         return self
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
